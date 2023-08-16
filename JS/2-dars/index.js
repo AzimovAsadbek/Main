dec9ba03ed1fff141970c61d517c5c1b9fe74243
AzimovAsadbek <asadbekazimov0629@gamil.com>
2023-08-16 18:15:25 +0930

    Dars homeworklari

---
+++ JS/2-dars/index.js
@@ -0,0 +1,27 @@
+// let a = 1, b = 1;
+// let c = ++a, d = b++;
+// console.log(c);
+// console.log(d++);
+// console.log(d);
+// =====
+// let a = 2;
+// let x = 1 + (a *= 2);
+// console.log(x)
+// =====
+// console.log("" - 1 + 0);
+// =====
+// console.log(true + false)
+// =====
+// console.log( "2");
+// console.log("3" * "2");
+// console.log(4 + 5 + "px");
+// console.log("$" + 4 + 5);
+// console.log("4px" - 2);
+// console.log("-9" + 5);
+// console.log("-9" - 5);
+// console.log(null + 1);
+// console.log(undefined + 1);
+// console.log(" /t /n" - 2);
+
+
+
