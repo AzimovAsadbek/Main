4639a5b696783e6cb9eebf5b76fa1087315ff9e2
AzimovAsadbek <asadbekazimov0629@gamil.com>
2023-08-18 03:08:20 +0930

    Darslar

---
+++ JS/1-dars/1-dars qollanma.js
@@ -0,0 +1,68 @@
+// Veriable => O'zgaruvchilar 
+// 1. var  => EcmaScript 5
+// 2. let => ES 6
+// 3. const => ES 6
+// let it = "hello"
+// const text = "hello js";
+// console.log(text);
+// Data types => Malumot turlari
+// Primitive / NonPrimitive
+// Primitive => Oddiy data type turi 
+//! String => "", '', ``
+// typeof => data type turini aniqlab beradi
+// let text1 = "hello 'World'"; // ES5
+// let text2 = 'hello "World"';//  ES5
+// let text3 = `hello
+//  "World"`; //  ES6
+// console.log(text1);
+// console.log(text2);
+// console.log( typeof text2);
+//! Number (-9007199254740992)-(+9007199254740992)
+// console.log(9007199254740992 + 5);
+//! Bigint 
+// belgisi n harfi
+// console.log(9007199254740992n + 7n);
+//! Boolean => Mantiqiy ma'lumot turi
+// Qiymatlari
+// 1. True => tog'ri
+// 2. False => noto'g'ri
+// console.log("5" == 5);
+//! Null => Biz bilib turib keyn ozartirish maqsadida null qiymatini berb ketsak boladi
+// let text = null;
+// console.log(text);
+// text = "webbrain"
+// console.log(text);
+//! Undifined => O'zgaruvchiga qiymat biriktirmay ketsak u Undifined qaytaradi
+// let a;
+// console.log(typeof a);
+//! Symbol => ES6 => for creating unique ID => qiymati takrorlanmaydigan ID yasash uchun
+// let a = Symbol
+// console.log(typeof Symbol);
+//! NonPrimitive => Oddiy bolmagan 
+//! Object => (object array function )
+//! object => bir odamga tegishli bolgan har hil malumotlarni oladi
+// let user = {
+//     age: 16,
+//     name: "shoxruh"
+// };
+// console.log(user.name);
+//! Array => Bir xil tudagi malumotlarni oladi
+// let b = ["shohruh", "shohjahon"];
+// let academy = [
+//     {},
+//     {}
+// ]
+// console.log(b[1]);
+//! Function => Funksiya
+// function face() {
+//     let a = 5 + 5;
+//     console.log(a);
+//     console.log("Hello");
+// }
+// face()
+//! Alert => Ekranga nalumot chiqarish
+// alert("Saytimizga hush kelibsiz!")
+
+
+
+
