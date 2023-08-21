b88dd1591426001ad45834de024c7c720d9206eb
Asadbek <asadbekazimov0629@gmail.com>
2023-08-21 13:27:31 +0930

    Homework

---
+++ JS/3-dars/index.js
@@ -0,0 +1,23 @@
+// let a = 2;
+// let x = 1 + (a *= 2)
+// console.log(x);//5
+
+// console.log("" + 1 + 0);// 10
+// console.log("" - 1 + 0);// -1
+// console.log(true + false);// 1
+// console.log(6 / "3");// 2
+// console.log("2" * "3");// 6
+// console.log(4 + 5 + "px"); // 9px
+// console.log("$" + 4 + 5);//$45
+// console.log("4" - 2);// 2
+// console.log(" - 9 " + 5);// - 9 5
+// console.log(" - 9 " - 5);// -14
+// console.log(null + 1);//1
+// console.log(undefined + 1);//NaN
+// console.log(" \t \n" - 2);// -2
+// console.log(5 > 4); // true
+// console.log("apple"> "pineapple");// false
+// console.log(undefined == null);//true
+// console.log(undefined === null);//false
+// console.log(null == "\n0\n");//false
+// console.log(null === +"\n0\n");//false
