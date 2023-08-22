92ad3a7f8715af672a005d817427f7c946801269
Asadbek <asadbekazimov0629@gmail.com>
2023-08-23 03:37:23 +0930

    Type Conversions

---
+++ JS/2. Type conversions 19-08/index.js
@@ -0,0 +1,26 @@
+// let a = 1, b = 1;
+// let c = ++a, d = b++;
+// console.log(c);// 2
+// console.log(d++);// 1
+// console.log(d);// 2
+// =====
+// let a = 2;
+// let x = 1 + (a *= 2);
+// console.log(x)//5
+// =====
+// console.log("" - 1 + 0);//-1
+// =====
+// console.log(true + false)// 1
+// =====
+// console.log("3" * "2");//6
+// console.log(4 + 5 + "px");//9px
+// console.log("$" + 4 + 5);//$45
+// console.log("4px" - 2);//NaN
+// console.log("-9" + 5);//-95
+// console.log("-9" - 5);// -14
+// console.log(null + 1);// 1
+// console.log(undefined + 1);NaN
+// console.log(" /t /n" - 2);//NaN
+
+
+
